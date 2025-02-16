6ceecf79344d2ce5407bac973d6a410d2dce8874
jannalmb00 <129775487+jannalmb00@users.noreply.github.com>
2025-02-15 19:51:54 -0500

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,13 @@
 $(document).ready(function(){
-  function toggleSection(sectionID, isShow){
-      if(isShow){
-          $(sectionID).addClass("active").fadeIn(500);
-          $(".section").not(sectionID).removeClass("active").fadeOut(300);
-      }else{
-        $(sectionID).addClass("active").fadeIn(500);
-      }
-  }
+  $("#project-btn").click(function() {  
+    window.location.href = "projects.html"; 
+  });
+  $("#skills-btn").click(function(){
+    window.location.href= "skills.html";
+  })
 
-  // Click event for the div    
-  $(".home-btn").on("click", function(){
-    toggleSection("#section2", true);
-  });
-  $("#project-btn").on("click", function(){
-    toggleSection("#section3", true);
-  });
-  $("#skills-btn").on("click", function(){
-    toggleSection("#section4", true);
-  });
-  $("#update-btn").on("click", function(){
-    toggleSection("#section5", true);
-  });
+
+  $("#home-btn").click(function(){
+    window.location.href="navigation.html";
+  })
 });
